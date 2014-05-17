c1de1ab23ccf54b448ce8a5a1938740f9e602653
PatrickWolleb <patrick.wolleb@shmedia.co.uk>
2014-05-17 20:56:15 +0100

    Added Hello

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -4,8 +4,11 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', 'ChatSocket', function($scope, ChatSocket) {
+ 	
+ 	$scope.test = 'Hello World' 		
+
   	ChatSocket.on('say', function(){
-  		debugger;
+
   	})
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
