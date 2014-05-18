444f13760509a4648d57818e9ccc468f9646355b
orliesaurus <orliesaurus@facebook.com>
2014-05-18 13:53:21 +0100

    commiting serverjs

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var sio
   , express = require('express')
   , http = require('http')
   , app = express()
-  , router = express.Router()
   , env = process.env.NODE_ENV || 'development'
   , port = process.env.PORT || 3000;
 
@@ -19,18 +18,23 @@ if (env == 'production') {
 }
 
 
+var auth = express.Router();
+
+auth.get('/', function (req, res) {
+    var tok = (Math.random() + Math.random() + Math.random() + Math.random() + Math.random()).toString().replace('.','');
+    res.send(200, {token: tok});
+});
+
+
+//APP ROUTES
+app.use("/auth", auth);
 app.use(express['static'](__dirname + '/public/app'));
-app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.redirect('app/index.html');
 });
 
-router.get('/auth', function (req, res) {
-    var tok = Math.random() + Math.random() + Math.random() + Math.random() + Math.random();
-    res.send({token: tok} );
-});
-
+//LAUNCH THE PONY
 
 server = http.createServer(app);
 sio = require('socket.io').listen(server);
@@ -38,13 +42,15 @@ chat = require('chat.io').createChat(sio.of('/chat'));
 
 sio.configure(function () {
   sio.of('/chat').authorization(function (handshake, callback) {
+    //Create an auth function that generates an auth-id and passes it on the header
+    handshake.auth= "AUTHGOESHERE";
     handshake.nickname = 'AloUser' + counter++;
+    console.log(handshake);
     callback(null, true);
   });
 });
 
 chat.on('join', function (user) {
-        console.log(user);
         message.append(user + ' joined your channel.');
     });
 
